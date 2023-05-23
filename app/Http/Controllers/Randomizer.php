52552f11410a9d4dac15b4b0e0699f0d396dbc1b
abitofBaileys <alexander@von-ehr.com>
2023-05-24 01:48:58 +0200

    Initial commit

---
+++ app/Http/Controllers/Randomizer.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\File;
+use Illuminate\View\View;
+use Mauricius\LaravelHtmx\Http\HtmxRequest;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Process;
+
+class Randomizer extends Controller
+{
+    // These are the valid file md5 checksums to accept
+    public array $MD5_hashes = [
+        ['hash' => '6efc477d6203ed2b3b9133c1cd9e9c5d', 'version' => 'NA'],
+        ['hash' => '026b649ed316448e038349e39a6fe579', 'version' => 'Fixxxer'],
+        ['hash' => 'b58c76f2ac0b2aeb9b779e880d2bff18', 'version' => 'Frue']
+    ];
+    /**
+     * @param HtmxRequest $request The request issued by the htmx form
+     */
+    public function makeRom(HtmxRequest $request)
+    {
+        if ($request->isHtmxRequest()) {
+
+            // Fetch the uploaded file
+            $uploadedFile = $request->file('file');
+            // Compare checksums
+            $fileCSUM = md5_file($uploadedFile);
+            $CSUM_valid = in_array($fileCSUM, array_column($this->MD5_hashes, 'hash'));
+            // If invalid checksum, return error message
+            if (!$CSUM_valid) {
+                return view()->renderFragment('output', 'error');
+            }
+
+            // Get original filename
+            $_originalFile = $uploadedFile->getClientOriginalName();
+            // Store filename and extension separately
+            $filename = pathinfo($_originalFile, PATHINFO_FILENAME);
+            $extension = pathinfo($_originalFile, PATHINFO_EXTENSION);
+            // Store the file in public storage under original filename
+            $path = Storage::putFileAs(
+                'public/roms',
+                $request->file('file'),
+                $_originalFile
+            );
+
+            // Get flags from form, if none specified, use v flag (Dummy flag: Randomize nothing)
+            $flags = implode($request->get('flag') ?? ["v"]);
+            // Get secret codes from form, if none specified, use no codes
+            $codes = implode($request->get('code') ?? [""]);
+            // Get seed from form, if none specified, use timestamp
+            $seed = ($request->get('seed') ?? time());
+            // Get randomness factor from form, if none specified use 0.5
+            $randomness = ($request->get('randomness') ?? "0.5");
+            // Get difficulty factor from form, if none specified use 1.0
+            $difficulty = ($request->get('difficulty') ?? "1.0");
+            // Chain arguments to a single string
+            $arguments = " ".$flags.$codes." ".$seed." ".$randomness." ".$difficulty;
+
+            // Build the command to execute
+            $command = "python " . base_path().'/vendor/abyssonym/terrorwave/randomizer.py ';
+            $command .= "'".storage_path('app') . "/" . $path."'".$arguments;
+
+            // Start the process
+            Process::path(storage_path('app'))->run($command);
+            // Build the file path to the randomized Rom file based on randomizer naming scheme
+            // "The randomizer will output a new, randomized rom with the seed in the filename."
+            // e.g. L2.sfc -> L2.stw.123456789.sfc
+            $outputFilePath = "storage/roms/" . $filename . '.' . $flags . '.' . $seed . '.' . $extension;
+            // Return a download button to this Rom file
+            return view()->renderFragment('output', 'download', [
+                'file_path' => $outputFilePath,
+                'file_name' => $filename . '.' . $flags . '.' . $seed . '.' . $extension,
+            ]);
+        } else {
+            dd("JavaScript is not activated or you didn't POST via htmx.");
+        }
+    }
+
+    /**
+     * @param HtmxRequest $request The request issued by the htmx form
+     */
+    public function deleteRoms(HtmxRequest $request)
+    {
+        if ($request->isHtmxRequest()) {
+            $foo = File::delete([
+                    $request->get('file_path'),
+                    $request->get('file_path_orig'),
+                    $request->get('file_path_spoiler'),
+                ]);
+            dd($foo);
+        } else {
+            dd("JavaScript is not activated or you didn't POST via htmx.");
+        }
+    }
+}
---
+++ resources/views/output.blade.php
@@ -0,0 +1,18 @@
+@fragment('download')
+    <a href="{{$file_path ?? ""}}" download="{{$file_name ?? ""}}" class="btn btn-primary p-0"
+    _="on click toggle my *display">
+        <span data-bs-dismiss="modal" class="d-block py-1 px-4">Download</span>
+    </a>
+@endfragment
+@fragment('error')
+    <p class="text-black">
+        <strong class="text-black">The uploaded ROM is not supported.</strong><br>
+        This randomizer requires a Lufia 2 (NA-SNES) rom with the MD5 hash listed below. The 'Lufia 2 Fixxxer Deluxe' version is also supported; vanilla Lufia 2 roms will automatically be patched with the 'Fixxxer Deluxe' patch.<br>
+        <br>
+        6efc477d6203ed2b3b9133c1cd9e9c5d (NA)<br>
+        026b649ed316448e038349e39a6fe579 (Fixxxer)<br>
+        b58c76f2ac0b2aeb9b779e880d2bff18 (Frue)<br>
+        <br>
+        <span class="btn btn-secondary" data-bs-dismiss="modal" class="d-block py-1 px-4">Close</span>
+    </p>
+@endfragment
